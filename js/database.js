acca80ab016d04e393a8c4c9cdb67af111576853
SteveSu55 <steve20130309@gmail.com>
2017-05-21 17:20:52 +0800

    Update

--- js/database.js
+++ js/database.js
@@ -1,31 +1,49 @@
 var fdb = new ForerunnerDB();
 var db = fdb.db("cars");
-
 var carsCollection = db.collection('cars');
-
-for (var i = 0;i = 0; i++) {
-	var newCars = {
-		name: "red" + i,
-		age: 10+i
-	}
-	carsCollection.insert(newCars)
-}
 carsCollection.save();
-
-carsCollection.load()
-
-function load(){
+carsCollection.load();
+$(document).ready(function(){
 	var cars = carsCollection.find();
-	console.log(cars)
+	console.log(cars);
+    for (var i = 0; i < cars.length; i++) {
+	    var carid = cars[i]._id;
+		var carname = cars[i].name;
+		var carcolor = cars[i].color;
+		$("#carid").append("<tr><button class='btn btn-link carsID' data-toggle='modal' data-target='#carModal'>" + carid + "</button></tr>");
+	    $("#carname").append("<tr>" + carname + "</tr>");
+	    $("#carcolor").append("<tr>" + carcolor + "</tr>");    
+	}
+    $(".carsID").click(function() {
+        var carsId = $(this).text();
+        console.log("carsId");
+        var query = {
+            _id: carsId
+        }
+        var cars = carsCollection.find(query)[0];
+        $("#carsName").text(cars.name);
+        $("#carsAge").text(cars.age);
+        $("#carsId").text(cars._id);
+        $("#carsInfo").modal('show');
+    });
+})
+function add() {
+    var carname = $("#carname").val();
+    var carcolor = $("#carcolor").val()
+    var newCars = {
+        name: carname,
+        color: carcolor
+    };
+    carsCollection.insert(newCars);
+    carsCollection.save();
+    var cars = carsCollection.find(newCars)[0]
+    var carid = cars._id;
+	var carname = cars.name;
+	var carcolor = cars.color;
+	console.log(carname);
+	console.log(carcolor);
+    $("#carid").append("<tr><button class='btn btn-link carsID' data-toggle='modal' data-target='#carModal'>" + carid + "</button><tr>");
+    $("#carname").append("<tr>" + carname + "</tr>");
+    $("#carcolor").append("<tr>" + carcolor + "</tr>");
 }
-
-setTimeout(load,1000)
-
-
-
-
-
-
-
-
-
+$("#add").click(add);
